c241518f5318bc0503764b5f211a90946383ac20
MinKyeom <minkyeom@naver.com>
2026-08-21 08:48:38 +0900

    Algorism Study Java

--- medianOfTwoSortedArray.java
+++ medianOfTwoSortedArray.java
@@ -1,23 +1,40 @@
+// 출처:출처: https://leetcode.com/problems/median-of-two-sorted-arrays/
 package TIL_Today_I_Leared;
 
+import java.util.List;
+import java.util.stream.Stream;
 import java.util.List;
 import java.util.stream.Stream;
 
 
 
-class medianOfTwoSortedArray{
+class medianOfTwoSortedArray {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         // List<String> list = new ArrayList<>(Arrays.asList(arr));
         // List<Integer> nums1List = Arrays.stream(nums1).boxed().toList();
-        // 컴파일 에러 발생
-        // Integer[] nums1 = nums1
-        
-        List<Integer> nums1List = new Array.stream(nums1).boxed().toList();
-        List<Integer> nums2List = new ArrayList<>(Arrays.asList(nums2));
 
-        List<Integer> combined = Stream.concat(nums1List.stream(),nums2List.stream()).toList();
+        List<Integer> nums1List = new ArrayList<>(Arrays.stream(nums1).boxed().toList());
+        // List<Integer> nums1List = new ArrayList<>(Arrays.asList(nums1));
+        List<Integer> nums2List = new ArrayList<>(Arrays.stream(nums2).boxed().toList());
+
+        List<Integer> combined = new ArrayList<>(Stream.concat(nums1List.stream(),nums2List.stream()).toList());
         System.out.println(combined);
-             
+        System.out.println( combined.size() );
+        combined.sort(null);
+
+
+        if(combined.size()%2==1){
+            int num = combined.size()/2;
+            return combined.get(num);
+        }
+        else{
+            // System.out.println("여기");
+            int num2 = combined.size()/2;
+            double result = (double)( combined.get(num2-1)+combined.get(num2) ) /2;
+            System.out.println(combined.get(num2));
+            return result;
+        }
+        
     }
 }
 

f59b9778a81175e25b23065c9f054b8a277045c4
MinKyeom <minkyeom@naver.com>
2026-08-24 08:50:26 +0900

    Algorism Study Python

---
+++ longest-palindromic-substring.py
@@ -0,0 +1,40 @@
+# 출처:https://leetcode.com/problems/longest-palindromic-substring/
+
+# 개선 중 
+
+from collections import deque,Counter
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        String_list = list(s)
+        before_list = deque([])
+        result_list = []
+
+        for String in String_list:
+            before_list.appendleft('')
+
+            new_list = deque([])
+            
+            for before in before_list:
+                before += String
+                new_list.append(String)
+            
+
+            before_list = new_list
+
+        print(before_list) 
+
+        result_num =0
+        result = ''
+
+        for check in before_list:
+
+            check_count = Counter(check)
+
+            # 앞 뒤가 같고, 총 개수가 2개
+            if check[0] == check[-1] and check_count[check[0]]==2:
+                if len(check) > result_num:
+                    result_num = len(check)
+                    result = check
+
+        return result
